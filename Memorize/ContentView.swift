a5664d7afa54a29dd48a66880ebf8159b69caba2
valera <valery.sig1@gmail.com>
2020-08-15 02:16:11 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Valery Sigal on 15/08/2020.
+//  Copyright © 2020 Valery Sigal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
