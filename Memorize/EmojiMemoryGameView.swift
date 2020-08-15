c7600da34db483223fc43c390f61b5964de0a69a
valera <valery.sig1@gmail.com>
2020-08-15 15:29:42 +0300

    [Lecture 3] Set the right font size Clean up the code as well

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -19,13 +19,8 @@ struct EmojiMemoryGameView: View {
                 }
             }
         }
-            .padding()
-            .foregroundColor(Color.orange)
-        .font(self.font)
-    }
-    
-    var font: Font {
-        viewModel.cards.count >= 10 ? Font.title : Font.largeTitle
+        .padding()
+        .foregroundColor(Color.orange)
     }
 }
 
@@ -33,15 +28,32 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
-            if card.isFaceUP {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            if self.card.isFaceUP {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
-        }.aspectRatio(2.0/3.0, contentMode: .fit)
+        }
+        .aspectRatio(2.0/3.0, contentMode: .fit)
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing constants
+    let cornerRadius = CGFloat(10)
+    let edgeLineWidth = CGFloat(3)
+    let fontScaleFactor = CGFloat(0.75)
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * fontScaleFactor
     }
 }
 
