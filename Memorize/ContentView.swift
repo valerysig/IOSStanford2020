f44b0c6b03b6298da9b08ddf9f4f7c5fcc049959
valera <valery.sig1@gmail.com>
2020-08-15 03:11:26 +0300

    [Lecture 1] Add multiple cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,10 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack(content: {
+            ForEach(0..<4, content: { index in
+                ZStack(content: {
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text("👻")
+                })
+            })
+        })
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
 
+// MARK: Glue the SwiftUI to the view
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
