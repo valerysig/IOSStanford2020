3cf6132a5c93414385426905b4138c9fc42be5b6
valera <valery.sig1@gmail.com>
2020-08-17 23:03:40 +0300

    [Lecture 4] Clean up code adding extension methods

---
+++ Memorize/Array+Identifiable.swift
@@ -0,0 +1,21 @@
+//
+//  Array+Identifiable.swift
+//  Memorize
+//
+//  Created by Valery Sigal on 17/08/2020.
+//  Copyright © 2020 Valery Sigal. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element: Identifiable {
+    func firstIndex(matching: Element) -> Int {
+        for index in 0..<self.count {
+            if self[index].id == matching.id {
+                return index
+            }
+        }
+        return 0 //TODO: bodus!!
+    }
+}
+	
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -25,18 +25,8 @@ struct MemoryGame<CardContent> {
     
     mutating func choose(card: Card) {
         print("card chosen: \(card)")
-        let chosenIndex: Int = self.index(of: card)
+        let chosenIndex: Int = cards.firstIndex(matching: card)
         self.cards[chosenIndex].isFaceUP = !self.cards[chosenIndex].isFaceUP
-    }
-    
-    func index(of card:Card) -> Int {
-        for index in 0..<self.cards.count {
-            if self.cards[index].id == card.id {
-                return index
-            }
-        }
-        
-        return -1 // TODO: bogus!
     }
     
     struct Card: Identifiable {
