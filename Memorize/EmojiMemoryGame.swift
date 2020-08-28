bed54a5a9e747d97534b1ef250fc9bf89c769e39
valera <valery.sig1@gmail.com>
2020-08-28 19:07:10 +0300

    [Assisgnment 3-5] Add new themes

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
-    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    static let EMOJI_THEME : [(name: String, cardsCount: Int, cardsColor: UIColor, emojis: [String])] = [
+        (name: "Halloween", cardsCount: Int.random(in: 2...5), cardsColor: UIColor.orange, emojis: ["👻", "🎃", "🕷", "☠️", "👽", "😈", "👹", "🤖", "👁", "👣", "🦂", "🦑"]),
+        (name: "Transportation", cardsCount: Int.random(in: 2...5), cardsColor: UIColor.blue, emojis: ["🚗", "🚕", "🚙", "🚌", "🏎", "🚓", "🚑", "🚒", "🚐", "🚚", "🚛", "🛵"]),
+        (name: "People", cardsCount: 12, cardsColor: UIColor.brown, emojis:["👩‍🦳", "👲", "👮", "👷️", "💂️", "🕵🏻‍️", "👩‍🌾", "👩‍🍳", "👩‍🎓", "👩‍🎤", "👨‍🎤", "👩‍💻"]),
+        (name: "Animals", cardsCount: 10, cardsColor: UIColor.green, emojis:["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐸", "🐵", "🐤", "🐔"]),
+        (name: "Weather", cardsCount: 8, cardsColor: UIColor.yellow, emojis:["☃️", "❄️", "🌨", "🌩", "☀️", "🌈", "🌪", "💫", "✨", "🌬", "💧", "☔️"]),
+        (name: "Fruits and vegetables", cardsCount: 6, cardsColor: UIColor.red, emojis:["🍏", "🍎", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🍒", "🥦", "🌶", "🥕"])
+    ]
     
+    @Published private var model: MemoryGame<String>
+    @Published private(set) var themeName: String
+    @Published private(set) var themeColor: UIColor
+
+    init() {
+        let modelAndTheme = EmojiMemoryGame.createMemoryGame()
+        self.model = modelAndTheme.0
+        self.themeName = modelAndTheme.1
+        self.themeColor = modelAndTheme.2
+    }
+
     // MARK: - Access to the Model
     var cards: Array<MemoryGame<String>.Card> {
         model.cards
@@ -22,12 +40,15 @@ class EmojiMemoryGame: ObservableObject {
     }
     
     // MARK: - Static Methods
-    static func createMemoryGame() -> MemoryGame<String> {
-        let emojis = ["👻", "🎃", "🕷", "☠️", "👽", "😈", "👹", "🤖", "👁", "👣", "🦂", "🦑"].shuffled()
+    static func createMemoryGame() -> (MemoryGame<String>, String, UIColor) {
+        // Theme: Theme name, Emoji sets, Number of cards to show, back color for cards
+        let emojiTheme = EMOJI_THEME[Int.random(in: 0..<EMOJI_THEME.count)]
+        let emojis = emojiTheme.emojis.shuffled()
         let numberOfPairs = Int.random(in: 2...5)
-        
-        return MemoryGame<String>(numberOfPairsOfCards: numberOfPairs) { pairIndex in
-            emojis[pairIndex]
-        }
+
+        return (MemoryGame<String>(numberOfPairsOfCards: numberOfPairs) { emojis[$0] },
+                emojiTheme.name,
+                emojiTheme.cardsColor
+        )
     }
 }
